e226caac4d9ab854cb4901d8928bc10c570b9214
Oleg <schika145@gmail.com>
2026-09-01 17:31:56 +0500

    почистили вектор

--- src/searchServer.cpp
+++ src/searchServer.cpp
@@ -80,20 +80,21 @@ std::vector<std::vector<RelativeIndex> > SearchServer::search(const std::vector<
                 genIndexes.push_back(genIndex);
             }
             queryEntres.clear();
-
-
-
-
+            for (int i = 0; i < genIndexes.size(); i++) {
+                if (genIndexes[i].rank == 0) {
+                    genIndexes.erase(genIndexes.begin() + i);
+                }
+            }
 
             result.push_back(genIndexes);
         }
-        for (auto gen_indexes: result) {
-            for (int i = 0; i < gen_indexes.size(); i++) {
-                if (gen_indexes[i].rank == 0) {
-                    gen_indexes.erase(gen_indexes.begin() + i);
-                }
-            }
-        }
+        // for (auto gen_indexes: result) {
+        //     for (int i = 0; i < gen_indexes.size(); i++) {
+        //         if (gen_indexes[i].rank == 0) {
+        //             gen_indexes.erase(gen_indexes.begin() + i);
+        //         }
+        //     }
+        // }
     } else {
         std::cout << "The number of requests exceeds the limit" << std::endl;
     }
